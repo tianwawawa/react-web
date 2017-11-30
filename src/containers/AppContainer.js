5565004cb45e51aa992da7c4d680f8d8d6b1aaa5
tianyq <tianyq@chyjr.com>
2017-11-30 13:55:20 +0800

    add flow . funny a little bug

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,10 +1,25 @@
+// @flow
 import React, { Component } from  'react'
+type Props = {
+  hello: boolean,
+};
 
-class AppContainer extends Component {
+class AppContainer extends Component<Props> {
+  constructor(props:Object){
+    super(props)
+    this.what = this.what.bind(this)
+  }
+   what (cute:boolean){
+     alert(cute)
+   }
+   componentWillamount(){
+    this.what(true)
+   }
+
   render () {
     return (
       <div>
-        <h5>what are you async </h5>
+        <h5>what are you async{this.props.hello} </h5>
       </div>
     )
 
